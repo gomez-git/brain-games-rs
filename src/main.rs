1229ebedc125215000e89f0cbf38a7290c2bee23
Konstantin Kuzin <wearebeard@gmail.com>
2023-07-03 18:35:12 +0300

    Add user name prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
     println!("Welcome to the Brain Games!");
+
+    print!("May I have your name? ");
+
+    io::stdout().flush().unwrap();
+
+    let mut username = String::new();
+
+    io::stdin()
+        .read_line(&mut username)
+        .expect("Something bad happened...");
+
+    let username = username.trim();
+
+    println!("Hello, {username}!");
 }
